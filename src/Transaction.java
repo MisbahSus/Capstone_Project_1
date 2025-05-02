94642129f9c5836025c9889978bfd6afcd28d310
MisbahSus <misbah859@gmail.com>
2025-05-02 14:43:48 -0700

    Transaction.java

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,4 +1,11 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Transaction {
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss");
+
     public String date, time, description, vendor;
     public double amount;
 
@@ -12,6 +19,19 @@ public class Transaction {
 
     @Override
     public String toString() {
+        return String.format("%s | %s | %-30s | %-20s | $%,.2f",
+                date, time, description, vendor, amount);
+    }
+
+    public String toCsvString() {
         return date + "|" + time + "|" + description + "|" + vendor + "|" + amount;
+    }
+
+    public LocalDate getLocalDate() {
+        return LocalDate.parse(date, DATE_FORMATTER);
+    }
+
+    public LocalTime getLocalTime() {
+        return LocalTime.parse(time, TIME_FORMATTER);
     }
 }
