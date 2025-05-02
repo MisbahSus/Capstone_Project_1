6eb142aaf74e33d4f7edb4ad105969431fdbc912
MisbahSus <misbah859@gmail.com>
2025-05-02 10:46:27 -0700

    Transaction.java

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class Transaction {
+    public String date, time, description, vendor;
+    public double amount;
+
+    public Transaction(String date, String time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return date + "|" + time + "|" + description + "|" + vendor + "|" + amount;
+    }
+
+    public String getDisplayString() {
+        String amountStr = String.format("$%.2f", amount);
+        return date + " | " + time + " | " + description + " | " + vendor + " | " + amountStr;
+    }
 }
